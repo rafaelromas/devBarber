8e8a97562147b82c761f664831d9a3d384bb673f
Rafael Roma Santana <rafael.roma10@gmail.com>
2023-04-26 19:17:59 -0300

    Adjusting JWT Token config and adjusting uses on routes file

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,8 +3,77 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use App\Models\User;
 
 class AuthController extends Controller
 {
     //
+
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['create', 'login']]);
+    }
+
+    public function create(Request $request)
+    {
+        $array = ['errors' => ''];
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if(!$validator->fails())
+        {
+            $name = $request->input('name');
+            $email = $request->input('email');
+            $password = $request->input('password');
+
+            $emailExists = User::where('email', $email)->count();
+
+            if($emailExists === 0)
+            {
+                $hash = password_hash($password, PASSWORD_DEFAULT);
+
+                $newUser = new User;
+                $newUser->name = $name;
+                $newUser-> email = $email;
+                $newUser->password = $hash;
+                $newUser->save();
+
+                $token = auth()->attempt([
+                    'email' => $email,
+                    'password' => $password
+                ]);
+
+                if(!$token)
+                {
+                    $array['errors'] = 'Ocorreu um erro!';
+                    return $array;
+                }
+
+                $info = auth()->user();
+                $array['data'] = $info;
+                $array['token'] = $token;
+
+            } else{
+                $array['errors'] = 'E-mail já cadastrado.';
+
+                return $array;
+            }
+
+        }else{
+
+            $array['errors'] = 'Dados incorretos.';
+
+            return $array;
+
+        }
+
+        return $array;
+
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BarberController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -19,3 +23,21 @@ Route::get("/ping", function(){
     return ["pong" => "true"];
 
 });
+
+Route::post('/auth/login', [AuthController::class, 'login']);
+Route::post('/auth/logout', [AuthController::class, 'logout']);
+Route::post('/auth/refresh', [AuthController::class, 'refresh']);
+
+Route::post('/user', [AuthController::class, 'create']);
+
+Route::get('/user', [UserController::class, 'read']);
+Route::put('/user', [UserController::class, 'update']); 
+Route::get('/user/favorites', [UserController::class, 'getFavorites']);
+Route::post('/user/favorite', [UserController::class, 'addFavorite']);
+Route::get('/user/appointments', [UserController::class, 'getAppointments']);
+
+Route::get('/barbers', [BarberController::class, 'list']);
+Route::get('/barbers/{id}', [BarberController::class, 'one']);
+Route::post('/barbers/{id}/appointment', [BarberController::class, 'setAppointment']);
+Route::get('/search', [BarberController::class, 'search']);
+
